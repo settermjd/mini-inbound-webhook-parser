d4866aa61a6968fffa326775fff08218d905ce5d
Matthew Setter <matthew@matthewsetter.com>
2024-07-29 15:49:23 +1000

    Add handler to download note as an attachment for an MMS
    This handler is used by the ProcessRequestHandler to retrieve the body
    of a note to be sent with the MMS as an attachment.

---
+++ src/GetNoteMessageBodyHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use JustSteveKing\StatusCode\Http;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+readonly class GetNoteMessageBodyHandler
+{
+    public function __construct(private DatabaseHandler $databaseHandler)
+    {}
+
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args
+) : ResponseInterface
+    {
+        $noteID = $request->getAttribute('note');
+        $note = $this->databaseHandler->getNoteByID($noteID);
+        if (is_null($note)) {
+            $response
+                ->getBody()
+                ->write(json_encode([
+                    "message" => "Note not found.",
+                    "detail" => "No note with note ID {$noteID} was found.",
+                ]));
+
+            return $response;
+        }
+
+        $response
+            ->getBody()
+            ->write($note['details']);
+
+        return $response
+            ->withHeader('content-disposition', 'attachment; filename=note.txt;')
+            ->withHeader('content-type', 'text/plain; charset=utf-8');
+    }
+}
---
+++ test/GetNoteMessageBodyHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace AppTest;
+
+use App\GetNoteMessageBodyHandler;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Slim\Psr7\Response;
+
+class GetNoteMessageBodyHandlerTest extends TestCase
+{
+    use DatabaseBackedTestTrait;
+
+    public function testCanRetrieveNoteMessageBodyForExistingNote()
+    {
+        $handler = new GetNoteMessageBodyHandler($this->handler);
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getAttribute')
+            ->with('note')
+            ->willReturn(1);
+
+        $response = $handler(
+            $request,
+            new Response(),
+            []
+        );
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(
+            'attachment; filename=note.txt;',
+            $response->getHeaderLine('content-disposition')
+        );
+        $this->assertSame(
+            'text/plain; charset=utf-8',
+            $response->getHeaderLine('content-type')
+        );
+    }
+
+    public function testReturnsErrorMessageWhenNoteDoesNotExist(): void
+    {
+        $handler = new GetNoteMessageBodyHandler($this->handler);
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getAttribute')
+            ->with('note')
+            ->willReturn(11);
+
+        $response = $handler(
+            $request,
+            new Response(),
+            []
+        );
+        $response->getBody()->rewind();
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(
+            [
+                'message' => 'Note not found.',
+                'detail' => 'No note with note ID 11 was found.',
+            ],
+            json_decode($response->getBody()->getContents(), true)
+        );
+    }
+}
